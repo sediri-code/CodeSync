2ee3bbe262f5a007757b26e90a3556d09fb295fc
sediri-code <63328434+sediri-code@users.noreply.github.com>
2023-07-05 18:46:58 +0200

    added forms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,27 @@
 <div class="flex justify-center">
 	<div class="grid grid-cols-1 gap-4 min-w-full md:min-w-[750px]">
 		<h3 class="text-center py-6">Create A code Snippet</h3>
-
+         <div class="card p-4 w-full text-token space-y-4">
+			<label class="label">
+				<span>Snippet Title</span>
+				<input class="input" type="text" placeholder="Entre Title" />
+			</label>
+			<label class="label">
+				<span>Programming language</span>
+				<select class="select">
+					<option value = "html">HTML</option> 
+					<option value = "CSS">CSS</option> 
+					<option value = "typescript">Typescript</option> 
+				</select>
+			</label>
+			<label class="label">
+				<span>Code Snippet</span>
+				<textarea class="textarea" rows="4" placeholder="Enter your code here"></textarea>
+			</label>
+		 </div>
+		 <div class = "text-center py-6">
+			<h2>My Code Snippets</h2>
+		 </div>
 	</div>
 
 </div>
